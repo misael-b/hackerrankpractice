d7f5eb97a1e12eb9f4a27d95202e8384332aee25
mbarcenas <misael.barcenas@yahoo.com>
2024-06-27 19:02:57 -0500

    salesbymatch

--- src/main/java/SalesByMatch.java
+++ src/main/java/SalesByMatch.java
@@ -1,40 +1,28 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SalesByMatch {
 
     public static int sockMerchant(int n, List<Integer> ar) {
         // Write your code here
-        if(n==0 || n==1){
-            return 0;
+        Map<Integer, Integer> valmap  = new HashMap<>();
+        for(int num : ar){
+            if(!valmap.containsKey(num)){
+                valmap.put(num, 1);
+            }else{
+                valmap.put(num, valmap.get(num) + 1);
+            }
         }
         int matches = 0;
-        int compareNum = ar.get(0);
-        while (ar.size() > 1){
-            for(int i = 1; i<ar.size(); i++) {
-                if (compareNum == ar.get(i)) {
-                    matches++;
-                    ar.remove((Integer) compareNum);
-                    ar.remove((Integer) compareNum);
-                    compareNum = ar.get(0);
-                    i = 0;
-                n= n-2;
-                }
+        System.out.println(valmap);
 
-
-
-//                i = 0;
-            }
-            if(!(ar.size() == 1) ){
-                ar.remove((Integer) compareNum);
-                compareNum = ar.get(0);
-            }
-
-
-
+        for(Integer match : valmap.values()){
+                matches = matches + (match /2);
         }
-        return matches;
+        return matches ;
 
 
 
