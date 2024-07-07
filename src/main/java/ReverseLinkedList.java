db9a52532c6d3a54719411abf8708f9501314e8e
mbarcenas <misael.barcenas@yahoo.com>
2024-07-07 16:28:48 -0500

    reverse linkedlist

--- src/main/java/ReverseLinkedList.java
+++ src/main/java/ReverseLinkedList.java
@@ -3,18 +3,23 @@ import java.io.IOException;
 
 public class ReverseLinkedList {
 
-
-
-
-
-
-
     public static SinglyLinkedListNode reverse(SinglyLinkedListNode llist) {
         // Write your code here
+        SinglyLinkedListNode previous = null;
+        SinglyLinkedListNode current = llist;
+
+
+        while (current != null) {
+            SinglyLinkedListNode next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        return previous;
 
 
     }
-            // Write your code here
 
 
 }
